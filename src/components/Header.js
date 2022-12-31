626278e2ae704e79402cc76c30e924cd1f81900e
Katie Chu <katiechux@gmail.com>
2022-12-30 20:14:47 -0800

    added more formatting to button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,31 @@
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import { Container, Navbar, NavbarBrand, Collapse, Nav, NavItem } from 'reactstrap';
 import logo from '../app/assets/spotchecklogo.png';
 
 const Header = () => {
     return(
-        <Navbar dark color='primary' sticky='top' expand='md'>
-            <Container>
-                <NavbarBrand href='/'>
-                    <img src={logo} alt='nucamp logo' className='logo'/>
-                </NavbarBrand>
-            </Container>
+        <Navbar sticky='top' expand='md' className='header pt-4'>
+            <NavbarBrand href='/'>
+                <img src={logo} alt='spotcheck logo' className='logo float-start mx-3'/>
+                <h2 className='mt-1'>Spotcheck</h2>
+            </NavbarBrand>
+            <Collapse isOpen='false' navbar>
+                <Container>
+                    <Nav className='justify-content-center' navbar>
+                        <NavItem className='nav-link link'>
+                            Features
+                        </NavItem>
+                        <NavItem className='nav-link link'>
+                            Resources
+                        </NavItem>
+                        <NavItem className='nav-link link'>
+                            Pricing
+                        </NavItem>
+                        <NavItem className='nav-link link'>
+                            About
+                        </NavItem>
+                    </Nav>
+                </Container>
+            </Collapse>
         </Navbar>
     );
 };
